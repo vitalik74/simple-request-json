eb5933077d29834575b316427d8a9a66105da941
vitalik74 <tsibikov_vit@mail.ru>
2015-03-24 15:54:43 +0500

    Add Request class

---
+++ Request.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Class Request
+ * @author Tsibikov Vitaliy <tsibikov_vit@mail.ru> <tsibikov.com>
+ * Create date: 24.03.2015 15:50
+ */
+
+namespace vitalik74\request;
+
+
+class Request 
+{
+
+}
